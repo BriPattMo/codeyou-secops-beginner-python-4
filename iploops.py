ad978fa9f20a097322abca008fd4ca0978bfdd65
BriPattMo <bri.pattmo@gmail.com>
2026-01-14 21:09:29 -0500

    Challenge 3 complete

--- iploops.py
+++ iploops.py
@@ -1,4 +1,4 @@
- 
+'''
 print("REPORT SCRIPT STARTED\n")
 
 ip_addresses = [
@@ -38,4 +38,45 @@ print(external_ips)
 print("\nREPORT SCRIPT ENDED")
 #print(f"Internal IPs = {internal_ips}")
 #print(f"External IPs = {external_ips}") 
+'''
+
+#CHALLANGE 3
+ip_addresses = []
+
+#Collect input until done
+while True:
+    ip = input("Enter an IP (or 'done' to finish): ").strip()
+
+    if ip.lower() == "done":
+        break
+
+    ip_addresses.append(ip)
+
+#Classify
+internal_ips = []
+external_ips = []
+
+index = 0
+while index < len(ip_addresses):
+    ip = ip_addresses[index]
+
+    if ip.startswith(("192.168.", "10.")):
+        internal_ips.append(ip)
+    else:
+        external_ips.append(ip)
+
+    index += 1
+
+#Report
+print("REPORT SCRIPT STARTED\n")
+print("IP Classifications")
+print("------------------------")
+print(f"Internal IPs ({len(internal_ips)}):")
+print(internal_ips)
+print()
+print(f"External IPs ({len(external_ips)}):")
+print(external_ips)
+print("\nREPORT SCRIPT ENDED")
+
+
 
